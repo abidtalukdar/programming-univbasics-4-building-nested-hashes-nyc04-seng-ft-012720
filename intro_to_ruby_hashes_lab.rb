809fb04cce2b2265e73da99d217dd5687f5f7617
Abid Talukdar <abiditalukdar@gmail.com>
2020-01-14 17:27:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,10 @@ end
 
 def monopoly_with_third_tier
   monopoly_hash = {
-	  :railroads => {:pieces => 4}
+	  :railroads => {
+	    :pieces => 4
+	    :names =>
+	  }
 	  :rent_in_dollars => {
 	    :one_piece_owned => 25,
 	    :two_pieces_owned => 50,
